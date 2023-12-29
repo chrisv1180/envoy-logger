e56f241cb9033650f91913bcc3e1bf79eab60a7f
Christian <chrisv1180-github@yahoo.com>
2023-12-29 13:17:10 +0100

    removed apscheduler.BackgroundScheduler

--- envoy_logger/__main__.py
+++ envoy_logger/__main__.py
@@ -1,16 +1,10 @@
 import logging
 import argparse
 import time
-from tzlocal import get_localzone
-
-
-from requests.exceptions import RequestException
 
 from envoy_logger import enphaseenergy
 from envoy_logger.sampling_loop import SamplingLoop
 from envoy_logger.cfg import load_cfg
-
-from apscheduler.schedulers.background import BackgroundScheduler
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,35 +28,9 @@ while True:
 
         S = SamplingLoop(envoy_token, cfg)
 
-        # if cfg.calc_hourly_data:
-        #     tz = get_localzone()
-        #     scheduler_hourly = BackgroundScheduler({'apscheduler.timezone': tz})
-        #     scheduler_hourly.add_job(S.write_to_influxdb_hourly, 'cron', hour="*", minute=0, second=0)
-        #     scheduler_hourly.start()
-        #
-        # if cfg.calc_daily_data:
-        #     tz = get_localzone()
-        #     scheduler_daily = BackgroundScheduler({'apscheduler.timezone': tz})
-        #     scheduler_daily.add_job(S.write_to_influxdb_daily, 'cron', hour=0)
-        #     scheduler_daily.start()
-
         S.run()
     except Exception as e:
         logging.error("%s: %s", str(type(e)), e)
-
-        # if cfg.calc_hourly_data:
-        #     try:
-        #         scheduler_hourly.shutdown(wait=False)
-        #     except Exception as e:
-        #         logging.warning("scheduler_hourly could not be shutdown correctly")
-        #         scheduler_hourly = None
-        #
-        # if cfg.calc_daily_data:
-        #     try:
-        #         scheduler_daily.shutdown(wait=False)
-        #     except Exception as e:
-        #         logging.warning("scheduler_daily could not be shutdown correctly")
-        #         scheduler_daily = None
 
         # sleep 5 minutes to recover
         time.sleep(300)
